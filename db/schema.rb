ea23b1325470a1cb184a956bb5aaee899dacd06c
Kat W-E <kat@fonix.com>
2024-12-31 15:51:16 +0000

    generate Participant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_31_154255) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_31_154955) do
   create_table "challenges", force: :cascade do |t|
     t.string "name"
     t.string "challenge_type"
@@ -36,6 +36,15 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_31_154255) do
     t.index ["user_id"], name: "index_logs_on_user_id"
   end
 
+  create_table "participations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "challenge_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["challenge_id"], name: "index_participations_on_challenge_id"
+    t.index ["user_id"], name: "index_participations_on_user_id"
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -55,5 +64,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_31_154255) do
 
   add_foreign_key "logs", "challenges"
   add_foreign_key "logs", "users"
+  add_foreign_key "participations", "challenges"
+  add_foreign_key "participations", "users"
   add_foreign_key "sessions", "users"
 end

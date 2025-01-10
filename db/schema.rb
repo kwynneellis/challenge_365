711132fbe8d700a955b1561b996e9950b368e45c
Kat W-E <kat@fonix.com>
2025-01-10 14:52:16 +0000

    rails gen partication model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_10_144522) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_10_145014) do
   create_table "challenges", force: :cascade do |t|
     t.string "name"
     t.string "challenge_type"
@@ -36,6 +36,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_10_144522) do
     t.index ["user_id"], name: "index_logs_on_user_id"
   end
 
+  create_table "participations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "challenge_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["challenge_id"], name: "index_participations_on_challenge_id"
+    t.index ["user_id"], name: "index_participations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -55,5 +64,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_10_144522) do
 
   add_foreign_key "logs", "challenges"
   add_foreign_key "logs", "users"
+  add_foreign_key "participations", "challenges"
+  add_foreign_key "participations", "users"
   add_foreign_key "users", "challenges", column: "primary_challenge_id"
 end

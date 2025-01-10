a78877175c9b5f0f68103db631adee6d0e4db3f5
Kat W-E <kat@fonix.com>
2025-01-10 14:43:22 +0000

    adds devise gem, user model and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_31_150423) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_10_143844) do
   create_table "challenges", force: :cascade do |t|
     t.string "name"
     t.string "challenge_type"
@@ -24,4 +24,23 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_31_150423) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.boolean "visibility", default: true
+    t.integer "primary_challenge_id"
+    t.boolean "reminder_email_opt_in", default: false, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["primary_challenge_id"], name: "index_users_on_primary_challenge_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "users", "challenges", column: "primary_challenge_id"
 end
